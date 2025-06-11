49c18e6fa55a09ecd75ea585ebe76f6d84ea0dcf
Kevin-A-L-E-X <kevinmontecinos2007@cesanrafael.org>
2025-06-11 19:05:05 -0400

    se creo el archivo requirements

--- config/conexion.py
+++ config/conexion.py
@@ -1,7 +1,7 @@
 import mysql.connector
 conexion=mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="1234",
-    database="bdventas"
+    host="bsgvikn7eu6up1yiox6x-mysql.services.clever-cloud.com",
+    user="uupezlfla5eky2uz",
+    password="0YWQWziRAozdLjB97jVM",
+    database="bsgvikn7eu6up1yiox6x"
 )
